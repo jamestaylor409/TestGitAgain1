785b8c998fb5bb9199c0de1e5d0b6e95212b5363
James Taylor <jamestaylor409@gmail.com>
2018-05-01 11:45:34 -0600

    Seeing is teh sync command is needed or is only git pushin command line will work

--- TestGitAgain1/Program.cs
+++ TestGitAgain1/Program.cs
@@ -10,6 +10,7 @@ namespace TestGitAgain1
             Console.WriteLine("Did this save?");
             Console.WriteLine("Hmm let's try again!");
             Console.WriteLine("It worked!");
+            Console.WriteLine("Will try is sync is needed after commit!");
         }
     }
 }
